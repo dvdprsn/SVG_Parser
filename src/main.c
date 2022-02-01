6f0bd437cb409014a06e37b10f82e441c81a84ba
David Pearson <dpears04@uoguelph.ca>
2022-01-31 21:33:53 -0500

    Added comments for explaination of issue

--- src/main.c
+++ src/main.c
@@ -8,7 +8,15 @@ int main(int argc, char **argv) {
         printf("Failed to create SVG\n");
     }
     
-    SVGToString(svg);
+    /**
+     * Current issue is that with SVGtoString running the height attribute prints as NULL in the testing print in deleteAttribute 
+     * after the struct passes through toString
+     * Once we go to deleteSVG it claims I am freeing memory that isnt there
+     * However, if SVGToString is commented out everything APPEARS to run as intended
+     * 
+     */
+    //SVGToString(svg);
+    //printf("%s\n",toString(svg->otherAttributes));
     deleteSVG(svg); 
 
     return 0;
