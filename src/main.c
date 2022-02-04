9b6cd97608e205ae0b9767be5709058564d0d60c
dpears04 <dpears04@uoguelph.ca>
2022-02-04 13:31:39 -0500

    Added new phrases for testing some get functions

--- src/main.c
+++ src/main.c
@@ -11,11 +11,24 @@ int main(int argc, char **argv) {
     //TODO check for failed mallocs and reallcos in code
 
     char *string = SVGToString(svg);
-
     printf("%s\n", string);
-
-    deleteSVG(svg); 
     free(string);
-    
+
+    printf("Get Rectanlges -----------\n");
+    List *rects = getRects(svg);
+    char *test = toString(rects);
+    printf("%s\n", test);
+    free(test);
+    freeList(rects);
+
+    printf("Get Circles --------\n");
+    List *circles = getCircles(svg);
+    test = toString(circles);
+    printf("%s\n",test);
+    free(test);
+    freeList(circles);
+
+    deleteSVG(svg);
+
     return 0;
 }
